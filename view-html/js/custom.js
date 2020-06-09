9361b301e13fbcd7f0b897c846d53636f49e8978
khanhk8a <khanhk8a@gmail.com>
2020-06-09 13:59:32 +0700

    ....

--- view-html/js/custom.js
+++ view-html/js/custom.js
@@ -1,81 +1,81 @@
 $(document).ready(function () {
-  // $(".carousel").carousel({
-  //   interval: 2000,
-  // });
-
-  $("#btn1").on("click", function () {
-    $("#tab1").css("display", "table");
-    $("#tab2").css("display", "none");
-    $(".tx_black2").removeClass("active");
-    $(".tx_black1").addClass("active");
-  });
-  $("#btn2").on("click", function () {
-    $("#tab2").css("display", "table");
-    $("#tab1").css("display", "none");
-    $(".tx_black1").removeClass("active");
-    $(".tx_black2").addClass("active");
-    console.log("hello");
-  });
-
-  $("input:checkbox").on("click", function () {
-    // in the handler, 'this' refers to the box clicked on
-    var $box = $(this);
-    console.log($box);
-    if ($box.is(":checked")) {
-      // the name of the box is retrieved using the .attr() method
-      // as it is assumed and expected to be immutable
-      var group = "input:checkbox[name='" + $box.attr("name") + "']";
-      console.log(group);
-      // the checked state of the group/box on the other hand will change
-      // and the current value is retrieved using .prop() method
-      $(group).prop("checked", false);
-      $box.prop("checked", true);
-    } else {
-      $box.prop("checked", false);
-    }
-  });
-  $(function () {
-    $(".date_select").each(function (index) {
-      var cmd = $(this);
-      var _filmId = $("#fiml_id_" + index).val();
-      var _date = $(this).val();
-      var url =
-        "/Film/LoadLichChieuPhim?filmId=" + _filmId + "&projectDate=" + _date;
-      cmd.change(function () {
-        //  alert('Hello word' + _filmId + _date);
-      });
-
-      $.get(url, function (data) {
-        $("#session_time_" + index).html(data);
-      });
-    });
-  });
-
-  $(".room_box_seat").each(function () {
-    var $rowLength = $(this).find("div.seat_line:first").find("a").length;
-    $(this)
-      .find("div.seat_line")
-      .attr("style", "width:" + $rowLength * 25 + "px");
-  });
-  $("#btn-ao-continue").click(function () {
-    // var _url = "/ShoppingCart/StartCheckout";
-    // if ($isProcess == true) {
-    //   alert(
-    //     "Xin bạn chờ đợi trong giấy lát hệ thống  đang thực hiện giao dịch trước đó của bạn"
-    //   );
-    //   return false;
-    // }
-    // $isProcess = true;
-    // $.get(_url, function (data) {
-    //   $isProcess = false;
-    //   if (data != undefined && data.redirect) {
-    //     location.href = data.redirect;
-    //   } else {
-    //     location.href = "/checkout";
-    //   }
+    // $(".carousel").carousel({
+    //   interval: 2000,
     // });
-    console.log("hello");
-  });
-  
+
+    $("#btn1").on("click", function () {
+        $("#tab1").css("display", "table");
+        $("#tab2").css("display", "none");
+        $(".tx_black2").removeClass("active");
+        $(".tx_black1").addClass("active");
+    });
+    $("#btn2").on("click", function () {
+        $("#tab2").css("display", "table");
+        $("#tab1").css("display", "none");
+        $(".tx_black1").removeClass("active");
+        $(".tx_black2").addClass("active");
+        console.log("hello");
+    });
+
+    $("input:checkbox").on("click", function () {
+        // in the handler, 'this' refers to the box clicked on
+        var $box = $(this);
+        console.log($box);
+        if ($box.is(":checked")) {
+            // the name of the box is retrieved using the .attr() method
+            // as it is assumed and expected to be immutable
+            var group = "input:checkbox[name='" + $box.attr("name") + "']";
+            console.log(group);
+            // the checked state of the group/box on the other hand will change
+            // and the current value is retrieved using .prop() method
+            $(group).prop("checked", false);
+            $box.prop("checked", true);
+        } else {
+            $box.prop("checked", false);
+        }
+    });
+    $(function () {
+        $(".date_select").each(function (index) {
+            var cmd = $(this);
+            var _filmId = $("#fiml_id_" + index).val();
+            var _date = $(this).val();
+            var url =
+                "/Film/LoadLichChieuPhim?filmId=" + _filmId + "&projectDate=" + _date;
+            cmd.change(function () {
+                //  alert('Hello word' + _filmId + _date);
+            });
+
+            $.get(url, function (data) {
+                $("#session_time_" + index).html(data);
+            });
+        });
+    });
+
+    $(".room_box_seat").each(function () {
+        var $rowLength = $(this).find("div.seat_line:first").find("a").length;
+        $(this)
+            .find("div.seat_line")
+            .attr("style", "width:" + $rowLength * 25 + "px");
+    });
+    $("#btn-ao-continue").click(function () {
+        // var _url = "/ShoppingCart/StartCheckout";
+        // if ($isProcess == true) {
+        //   alert(
+        //     "Xin bạn chờ đợi trong giấy lát hệ thống  đang thực hiện giao dịch trước đó của bạn"
+        //   );
+        //   return false;
+        // }
+        // $isProcess = true;
+        // $.get(_url, function (data) {
+        //   $isProcess = false;
+        //   if (data != undefined && data.redirect) {
+        //     location.href = data.redirect;
+        //   } else {
+        //     location.href = "/checkout";
+        //   }
+        // });
+        console.log("hello");
+    });
+
 
 });
--- view-html/js/lienhe.js
+++ view-html/js/lienhe.js
@@ -1,29 +1,28 @@
-$(document).ready(function(){
-    $(".block-cms-menu li").each(function(){
+$(document).ready(function () {
+    $(".block-cms-menu li").each(function () {
         var self = this;
-        href = $(this).find('a');	
-        for(var i = 0 ; i < href.length ; i++){
-        var harray = $(href[i]).attr('href').split("/");
-        var charray = window.location.href.split("/");
-        if(harray[harray.length-2] == charray[charray.length-2]){
-        $(self).addClass('active');
-        break;
+        href = $(this).find('a');
+        for (var i = 0; i < href.length; i++) {
+            var harray = $(href[i]).attr('href').split("/");
+            var charray = window.location.href.split("/");
+            if (harray[harray.length - 2] == charray[charray.length - 2]) {
+                $(self).addClass('active');
+                break;
+            }
         }
-        }			
-        });
+    });
 
-        var message = 'vui lòng cung cấp thông tin rạp, email và số điện thoại thành viên mà bạn đã dùng để đặt vé.';
-        var contactForm = new VarienForm('contactForm', true);
-        var hoadd = $('.address-contact-cgv-block-content').html();
-        var holocation = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25615.906364793133!2d106.70663701731654!3d10.78025381605616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf16ffaa20e46e87f!2sCJ+CGV+Vietnam!5e0!3m2!1sen!2s!4v1459915713967';
-        $('.location-ho-cgv iframe').attr('src',holocation);
-        
-        function inquiriyf(obj) {
-        if( $j(obj).find('option:selected').attr('data-value') == 2) {
-        $('#comment').attr('placeholder', message);
+    var message = 'vui lòng cung cấp thông tin rạp, email và số điện thoại thành viên mà bạn đã dùng để đặt vé.';
+    var contactForm = new VarienForm('contactForm', true);
+    var hoadd = $('.address-contact-cgv-block-content').html();
+    var holocation = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25615.906364793133!2d106.70663701731654!3d10.78025381605616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf16ffaa20e46e87f!2sCJ+CGV+Vietnam!5e0!3m2!1sen!2s!4v1459915713967';
+    $('.location-ho-cgv iframe').attr('src', holocation);
+
+    function inquiriyf(obj) {
+        if ($j(obj).find('option:selected').attr('data-value') == 2) {
+            $('#comment').attr('placeholder', message);
+        } else {
+            $('#comment').attr('placeholder', '');
         }
-        else {
-        $('#comment').attr('placeholder','');
-        }
-        }
+    }
 });
